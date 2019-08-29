83649e30a484b21456455cce70b38077a3387167
wangyilin <wangyilin@wangyilins-MacBook-Pro.local>
2019-08-29 16:32:05 +0800

    add pico core dep

--- cmake/FindPicoCoreDep.cmake
+++ cmake/FindPicoCoreDep.cmake
@@ -1,6 +1,6 @@
 include(common)
 
-find_lib(PicoCoreDep_LIBRARIES STATIC LIBS glog gflags boost_regex yaml-cpp boost_iostreams boost_system boost_thread zookeeper_mt z snappy lz4)
+find_lib(PicoCoreDep_LIBRARIES STATIC LIBS glog gflags boost_regex yaml-cpp boost_iostreams boost_system boost_filesystem boost_thread zookeeper_mt z snappy lz4 )
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(PicoCoreDep DEFAULT_MSG PicoCoreDep_LIBRARIES)
