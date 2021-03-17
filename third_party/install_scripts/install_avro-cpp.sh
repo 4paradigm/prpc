baba346e4762ba68bfb83b66f1bf3d9a325748e8
wangyilin <wangyilin@4paradigm.com>
2021-03-17 20:49:29 +0800

    add avrocpp

---
+++ third_party/install_scripts/install_avro-cpp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+source ${install_script_dir}/common.sh
+
+
+#function install_avro() {
+#    name="avro-cpp"
+#    install_pkg_common_preprocess "$name"
+#    if [ $? -eq 0 ]; then
+#        #patch -f -p3 < ${prefix}/pkgs/avro.git*
+#        patch -f -p1 < ${prefix}/pkgs/avro.pico.diff
+#        execshell "mkdir _build" && execshell "pushd _build"
+#        execshell "$CMAKE .. -DCMAKE_INSTALL_PREFIX=$prefix $cmake_exdefine"
+#        execshell "make -j$J"
+#        [ "X$install_cmd" = "X" ] && execshell "make install" || execshell "$install_cmd"
+#        execshell "popd"
+#        install_pkg_common_postprocess "$name"
+#    fi
+#    return 0
+#}
+
+pkg_patch_list="${patches_dir}/patch-avro.patch" \
+    cmake_preprocess="patch -f -p1 < ${patches_dir}/patch-avro.patch" \
+    cmake_exdefine="-DCMAKE_CXX_FLAGS=-fPIC -DCMAKE_C_FLAGS=-fPIC -DCMAKE_POSITION_INDEPENDENT_CODE=ON" \
+    execshell "install_cmake_pkg avro-cpp"
+
