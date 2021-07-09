19b98ef29b4016c21add89a8dae79c4eb8f40a6e
wangyilin <wangyilin@4paradigm.com>
2021-07-09 16:36:18 +0800

    github cicd

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,7 @@ RUN apt-get update && apt-get install -y g++-7  openssl curl wget git \
 RUN apt-get update && apt-get install -y libsnappy-dev libprotobuf-dev libprotoc-dev libleveldb-dev \
     zlib1g-dev liblz4-dev libssl-dev libzookeeper-mt-dev libffi-dev libbz2-dev liblzma-dev
 
-ENV TENSORFLOW=2.2
-ENV HOROVOD_GPU_ALLREDUCE=NCCL
-
-ADD . /pico-core
-ENV GITHOST=hub.fastgit.org
-RUN bash /pico-core/third_party/prepare.sh build gflags glog googletest sparsehash zlib snappy lz4 boost yaml jemalloc prometheus-cpp avro-cpp brpc
-WORKDIR /pico-core
-RUN mkdir build && cmake .. && make -j && make test && make install && cd ..
+ADD . /prpc
+RUN bash /prpc/third_party/prepare.sh build gflags glog googletest sparsehash zlib snappy lz4 boost yaml jemalloc prometheus-cpp avro-cpp brpc
+WORKDIR /prpc
+RUN mkdirs build && cd build && cmake .. && make -j && make test && make install && cd ..
--- build.sh
+++ build.sh
@@ -3,22 +3,14 @@ set -e
 PROJECT_ROOT=`pwd`
 echo ${PROJECT_ROOT}
 
-function build() {
-    mkdir -p ${PROJECT_ROOT}/build
-    pushd ${PROJECT_ROOT}/build
-    cmake ../
-    make -j$J
-    popd
-}
-
 function publish() {
     if [ 0"${REGISTRY}" == "0" ]; then
         echo "REGISTRY not set"
         return 1
     fi
-    git_tag = `git describe --exact-match HEAD || git rev-parse HEAD`
+    tag=`git describe --exact-match HEAD || git rev-parse HEAD`
     if [ 0"${VERSION}" == "0" ]; then
-        VERSION=${git_tag}
+        VERSION=${tag}
     fi
     echo "tag=${tag}"
     echo "REGISTRY=${REGISTRY}"
@@ -44,22 +36,10 @@ function publish_check() {
     fi
 }
 
-function unit_test() {
-    pushd ${PROJECT_ROOT}/build
-    make test
-    popd
-}
-
 case "$1" in
-    test)
-        unit_test
-    ;;
     publish)
         publish_check
         publish
-    ;;
-    build|"")
-        build
     ;;
     *)
         echo -e "unkown cmd"
